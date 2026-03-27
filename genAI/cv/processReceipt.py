3005dd32021a42dc2d0f3a54119042eb685ac0ad
Sahil Shah <sahilshah2104@gmail.com>
2026-03-27 23:00:26 +0530

    Feature: Backend setup and GenAI initialisation completed

---
+++ genAI/cv/processReceipt.py
@@ -0,0 +1,10 @@
+import pytesseract
+
+class ProcessReceipts:
+    def __init__():
+        pass
+
+    def convertImageToData() -> dict:
+        # TODO: Write the logic to extract data from the receipt image and send back
+        receiptData: dict = {}
+        return receiptData
--- genAI/main.py
+++ genAI/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+import json
+
+from cv.processReceipt import ProcessReceipts
 
 app = FastAPI()
 print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
@@ -8,3 +11,20 @@ print("━━━━━━━━━━━━━━━━━━━━━━━━�
 @app.get("/health")
 def read_health():
     return {"message": "GenAI Service running"}
+
+@app.post("/uploadreceipt")
+def process_uploaded_receipt(request):
+    # convert the request json in python dictionary
+    requestData: dict = json.loads(request)
+
+    # initalising the CV Service handler
+    cvHandler = ProcessReceipts()
+    receiptData: dict = cvHandler.convertImageToData(requestData)
+
+@app.get("/generatesummary")
+def generate_llm_summary():
+    pass
+
+@app.get("/getanalytics")
+def get_user_analytics():
+    pass
